d449091dd4fd9d7dbb4bc03bac65b75f95bd0555
Amias-Burrows <amias.burrows@gmail.com>
2021-03-17 11:21:54 +0000

    ADD MORE FUNCTIONS

---
+++ javascript/burrows.js
@@ -0,0 +1,11 @@
+		//Nav bar
+
+var list = document.getElementsByClassName('dropdown-list');
+
+document.getElementsByClassName('dropdown-link').addEventListener("click", drop() {
+	if (list.display = none) {
+		list.display = inline;
+	} else {
+		list.display = none;
+	}
+});
